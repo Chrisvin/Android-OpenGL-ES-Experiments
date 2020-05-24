a5633fbaf9f7fd0721172385e1946a2187cf55fc
Jem <chrisvin1997@gmail.com>
2020-05-24 06:26:04 +0530

    Add simple android app

---
+++ app/src/main/java/com/jem/openglexperiment/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.jem.openglexperiment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //TODO: Add intent redirection to other OpenGL Experiment activities once they're ready.
+    }
+}
