12f106d11ff371c57b2dbf247cd3b247ff186676
Jem <chrisvin1997@gmail.com>
2020-05-27 03:01:53 +0530

    Add scale handling in experiment 4

--- app/src/main/java/com/jem/openglexperiment/opengl/experiments/experiment3/GLSurfaceView3.kt
+++ app/src/main/java/com/jem/openglexperiment/opengl/experiments/experiment3/GLSurfaceView3.kt
@@ -2,13 +2,29 @@ package com.jem.openglexperiment.opengl.experiments.experiment3
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import com.jem.openglexperiment.opengl.base.BaseGLSurfaceView
 
-class GLSurfaceView3: BaseGLSurfaceView {
+class GLSurfaceView3 : BaseGLSurfaceView {
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 
+    private val scaleGestureDetector =
+        ScaleGestureDetector(context, object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+            override fun onScale(detector: ScaleGestureDetector?): Boolean {
+                detector?.scaleFactor?.let {
+                    (glRenderer as? GLSurfaceViewRenderer3?)?.updateScaleFactor(it - 1f)
+                }
+                return true
+            }
+        })
+
     override fun getRendererInstance(): Renderer {
         return GLSurfaceViewRenderer3()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return scaleGestureDetector.onTouchEvent(event)
     }
 }
--- app/src/main/java/com/jem/openglexperiment/opengl/experiments/experiment3/GLSurfaceViewRenderer3.kt
+++ app/src/main/java/com/jem/openglexperiment/opengl/experiments/experiment3/GLSurfaceViewRenderer3.kt
@@ -7,9 +7,11 @@ import com.jem.openglexperiment.opengl.shapes.FourTriangles
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
-class GLSurfaceViewRenderer3: BaseGLSurfaceViewRenderer() {
+class GLSurfaceViewRenderer3 : BaseGLSurfaceViewRenderer() {
 
     private lateinit var triangle: FourTriangles
+    private val scaleMatrix = FloatArray(16)
+    private var scaleFactor = 1f
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         super.onSurfaceCreated(gl, config)
@@ -17,10 +19,12 @@ class GLSurfaceViewRenderer3: BaseGLSurfaceViewRenderer() {
     }
 
     override fun onDrawFrame(gl: GL10?) {
-//        super.onDrawFrame(gl)
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-        Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, -5f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
-        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
-        triangle.draw(vPMatrix)
+        super.onDrawFrame(gl)
+        Matrix.scaleM(scaleMatrix, 0, vPMatrix, 0, scaleFactor, scaleFactor, scaleFactor)
+        triangle.draw(scaleMatrix)
+    }
+
+    fun updateScaleFactor(scaleFactorDiff: Float) {
+        this.scaleFactor += scaleFactorDiff
     }
 }
