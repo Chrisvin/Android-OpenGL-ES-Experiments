2e1998d2976abf3d9745a87612b0528a3be9f3ee
Jem <chrisvin1997@gmail.com>
2020-05-28 04:50:28 +0530

    Add simple bottom left triangle implementation of BaseFragmentShader

--- app/src/main/java/com/jem/openglexperiment/opengl/base/BaseFragmentShader.kt
+++ app/src/main/java/com/jem/openglexperiment/opengl/base/BaseFragmentShader.kt
@@ -1,19 +1,79 @@
 package com.jem.openglexperiment.opengl.base
 
+import android.opengl.GLES20
+import android.os.SystemClock
+import com.jem.openglexperiment.opengl.helper.FloatBufferHelper
 import com.jem.openglexperiment.opengl.helper.ShaderHelper
 
 abstract class BaseFragmentShader {
 
+    companion object {
+        protected const val COORDS_PER_VERTEX = 3
+        protected const val BYTES_PER_VERTEX = 4
+        protected const val VERTEX_STRIDE = COORDS_PER_VERTEX * BYTES_PER_VERTEX
+
+        protected const val COMMON_SHADER_PARAMS = "uniform float vTime;"
+    }
+
     protected var program: Int = 0
-    private var fragmentShaderHandle: Int = 0
+
+    private var positionHandle = 0
+
+    private val vertexArray = floatArrayOf(
+        -1f, 1f, 0f,
+        -1f, -1f, 0f,
+        1f, -1f, 0f
+    )
+    private val vertexBuffer by lazy {
+        FloatBufferHelper.allocateDirectBuffer(
+            this.vertexArray.size * BYTES_PER_VERTEX,
+            this.vertexArray
+        )
+    }
+
+    private val vertexShaderCode: String = "" +
+            "attribute vec4 vPosition;" +
+            COMMON_SHADER_PARAMS +
+            "void main() {" +
+            "   gl_Position = vPosition;" +
+            "}"
+
+    private val startTime: Long = SystemClock.uptimeMillis()
 
     init {
-        fragmentShaderHandle = ShaderHelper.loadFragmentShader(this.getFragmentShaderCode())
-        program = ShaderHelper.createProgram(fragmentShader = fragmentShaderHandle)
+        val vertexShaderHandle = ShaderHelper.loadVertexShader(vertexShaderCode)
+        val fragmentShaderCode =
+            this.getFragmentShaderCode()
+                .replace("void main()", COMMON_SHADER_PARAMS + "void main()")
+        val fragmentShaderHandle = ShaderHelper.loadFragmentShader(fragmentShaderCode)
+        program = ShaderHelper.createProgram(vertexShaderHandle, fragmentShaderHandle)
     }
 
     abstract fun getFragmentShaderCode(): String
+    abstract fun handleFragmentAttributes()
 
-    abstract fun draw()
+    fun draw() {
+        GLES20.glUseProgram(program)
+
+        GLES20.glGetUniformLocation(program, "vTime").also {
+            GLES20.glUniform1f(it, (SystemClock.uptimeMillis() - startTime).toFloat())
+        }
+        positionHandle = GLES20.glGetAttribLocation(program, "vPosition").also {
+            GLES20.glEnableVertexAttribArray(it)
+            GLES20.glVertexAttribPointer(
+                it,
+                COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT,
+                false,
+                VERTEX_STRIDE,
+                vertexBuffer
+            )
+        }
+
+        handleFragmentAttributes()
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexArray.size / COORDS_PER_VERTEX)
+        GLES20.glDisableVertexAttribArray(positionHandle)
+    }
 
 }
