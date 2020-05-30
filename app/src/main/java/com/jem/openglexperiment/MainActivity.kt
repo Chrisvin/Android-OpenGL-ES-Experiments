56b6988048dce79f19f10b716eab9879bb557f0e
Jem <chrisvin1997@gmail.com>
2020-05-31 04:44:33 +0530

    Add intent redirections from MainActivity

--- app/src/main/java/com/jem/openglexperiment/MainActivity.kt
+++ app/src/main/java/com/jem/openglexperiment/MainActivity.kt
@@ -1,7 +1,19 @@
 package com.jem.openglexperiment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.jem.openglexperiment.opengl.base.BaseGLActivity
+import com.jem.openglexperiment.opengl.experiments.experiment1.GLActivity1
+import com.jem.openglexperiment.opengl.experiments.experiment2.GLActivity2
+import com.jem.openglexperiment.opengl.experiments.experiment3.GLActivity3
+import com.jem.openglexperiment.opengl.experiments.experiment4.GLActivity4
+import com.jem.openglexperiment.opengl.experiments.experiment5.GLActivity5
+import com.jem.openglexperiment.opengl.experiments.experiment6.GLActivity6
+import com.jem.openglexperiment.opengl.experiments.experiment7.GLActivity7
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +21,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //TODO: Add intent redirection to other OpenGL Experiment activities once they're ready.
+        addActivityButtons(
+            GLActivity1::class.java,
+            GLActivity2::class.java,
+            GLActivity3::class.java,
+            GLActivity4::class.java,
+            GLActivity5::class.java,
+            GLActivity6::class.java,
+            GLActivity7::class.java
+        )
     }
+
+    private fun addActivityButtons(vararg activities: Class<out BaseGLActivity>) {
+        activities.forEachIndexed { index, activity ->
+            val button = Button(this@MainActivity).apply {
+                setOnClickListener {
+                    startActivity(Intent(this@MainActivity, activity))
+                }
+                text = "Experiment ${index + 1}"
+            }
+            activityList.addView(
+                button,
+                LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+            )
+        }
+    }
+
 }
