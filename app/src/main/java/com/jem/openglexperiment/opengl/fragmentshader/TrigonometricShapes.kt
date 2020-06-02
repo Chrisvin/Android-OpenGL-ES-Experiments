e595287e94afab1a77c6f4eadac5b6688adccd46
Jem <chrisvin1997@gmail.com>
2020-06-03 04:29:47 +0530

    Add TrigonometricShapes fragmentShader

---
+++ app/src/main/java/com/jem/openglexperiment/opengl/fragmentshader/TrigonometricShapes.kt
@@ -0,0 +1,45 @@
+package com.jem.openglexperiment.opengl.fragmentshader
+
+import android.opengl.GLES20
+import com.jem.openglexperiment.opengl.base.BaseFragmentShader
+
+class TrigonometricShapes : BaseFragmentShader() {
+
+    private var innerTrigFactor: Float = 12f
+
+    override fun getFragmentShaderCode(): String {
+        return "" +
+                "precision mediump float;" +
+
+                "uniform float innerTrigFactor;" +
+
+                "void main(){" +
+                "   vec3 color = vec3(0.620,0.041,0.094);" +
+                "   vec2 pos = vec2(0.5, 0.9) - gl_FragCoord.xy/vResolution.x;" +
+
+                "   float r = length(pos)*2.0;" +
+                "   float a = atan(pos.y,pos.x);" +
+
+                "   float f = smoothstep(-0.5, -0.0, cos(a*10. + vTime * 8.0))*0.2+0.5;" +
+                "   color = max(color, mix(color, vec3(0.,0., 1.-smoothstep(f,f+0.4,r)), .75));" +
+
+                "   f = abs(cos(a*12. - vTime * 5.0)*sin(a*3. - vTime * 4.))*0.5+.31;" +
+                "   color = max(color, mix(color, vec3(1.-smoothstep(f,f+0.2,r / 0.65)), 0.5));" +
+
+                "   f = ceil(sin(a*3. - vTime * 2.0)*sin(a*1.0 + vTime * 4.0))*0.5+.4;" +
+                "   color = max(color, mix(color, vec3(1.-smoothstep(f,f+0.2,r / 0.2)), 0.5 ));" +
+
+                "   gl_FragColor = vec4(color, 1.0);" +
+                "}"
+    }
+
+    override fun handleFragmentAttributes() {
+        GLES20.glGetUniformLocation(program, "innerTrigFactor").also {
+            GLES20.glUniform1f(it, innerTrigFactor)
+        }
+    }
+
+    fun updateScaleFactor(scaleFactor: Float) {
+        innerTrigFactor += scaleFactor
+    }
+}
